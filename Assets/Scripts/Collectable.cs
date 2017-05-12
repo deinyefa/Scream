583ea3948b464f74d31b7f08c1698b8a32c0e682
Deinyefa Eporwei <df.eporwei@gmail.com>
2017-05-12 11:34:00 -0400

    setup collectables throughout the scene

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour {
+
+    public GameObject[] collectables;
+
+    public bool AreCollectablesInactive ()
+    {
+        foreach (GameObject collectable in collectables)
+        {
+            if (collectable.gameObject.activeSelf == true)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
