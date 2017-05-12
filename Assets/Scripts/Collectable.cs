c661e6ac1b6b27858809d3976df75af282aa4715
Deinyefa Eporwei <df.eporwei@gmail.com>
2017-05-12 12:52:40 -0400

    key disappears when player touches it

--- Assets/Scripts/Collectable.cs
+++ Assets/Scripts/Collectable.cs
@@ -4,17 +4,5 @@ using UnityEngine;
 
 public class Collectable : MonoBehaviour {
 
-    public GameObject[] collectables;
 
-    public bool AreCollectablesInactive ()
-    {
-        foreach (GameObject collectable in collectables)
-        {
-            if (collectable.gameObject.activeSelf == true)
-            {
-                return false;
-            }
-        }
-        return true;
-    }
 }
