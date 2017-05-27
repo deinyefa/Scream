0f94f1f5e774c033f71a762f20d44522f2e2d008
Deinyefa Eporwei <df.eporwei@gmail.com>
2017-05-27 15:58:11 -0400

    reset player position

---
+++ Assets/Scripts/Reset.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reset : MonoBehaviour {
+
+	public GameObject player;
+	private Vector3 initialPlayerPosition;
+
+	void Awake ()
+	{
+		initialPlayerPosition = player.transform.position;
+	}
+
+	void OnTriggerEnter (Collider other) {
+		//- reset player position if player falls off the platform
+		other.transform.position = initialPlayerPosition;
+	}
+}
